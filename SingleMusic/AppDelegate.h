9a23d3ed5903128a6cf28d2c0913d2473fb81a74
FJW-GitHub <xiaofu@xiaofudemacbook-pro.local>
2016-03-17 09:03:35 +0800

    Initial Commit

---
+++ SingleMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleMusic
+//
+//  Created by xiaofu on 16/3/17.
+//  Copyright © 2016年 xiaofu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
